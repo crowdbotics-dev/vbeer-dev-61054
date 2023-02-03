b32889dab61804d5b8c5c5347a3a63386c43ba70
Crowdbotics <engineering@crowdbotics.com>
2023-02-03 10:32:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/vbeer_dev_61054/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vbeer_dev_61054;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "vbeer_dev_61054";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "vbeer-dev-61054",
+  url: "https://vbeer-dev-61054.botics.co",
+  api: "https://vbeer-dev-61054.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
